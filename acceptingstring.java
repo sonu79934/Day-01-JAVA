cbb7580628d04973dbe7e0d743466814e1c9924b
23HR1AO4C6 <yogasritha8@gmail.com>
2024-12-02 15:38:41 +0530

    Add files via upload

---
+++ acceptingstring.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class acceptingstring {
+    public static void main(String[] args){
+        //Accepting a string
+        Scanner sc = new Scanner(System.in);
+        String a=sc.next();
+        String b=sc.next();
+        String c=sc.next();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+}
