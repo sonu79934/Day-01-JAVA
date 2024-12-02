d74b9f5f85a7450102375b6c60e19f70838a1a22
23HR1AO4C6 <yogasritha8@gmail.com>
2024-12-02 15:18:22 +0530

    Add files via upload

---
+++ printingnextchar.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class printingnextchar {
+    public static void main(String[] args) {
+        char c ='a';
+        int b=c;
+        b+=1;
+        char v=(char)b;
+        System.out.println(v);
+    }
+}
