144e1ac33c9aaab9986d4017c690f253f309d2a5
23HR1AO4C6 <yogasritha8@gmail.com>
2024-12-02 14:42:53 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class datatypes {
+    public static void main(String[] args){
+        int a=12;
+        char b='A';
+        float c=2.14f;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
