5987f9ac32297ddc0cfd3189277ba87c1b14addd
23HR1AO4C6 <yogasritha8@gmail.com>
2024-12-02 15:03:09 +0530

    Add files via upload

---
+++ typecasting.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class typecasting {
+    public static void main(String[] args){
+        //conversion of acii value to char
+        int a=50;
+        char b=(char)(a);
+        char c='7';
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
