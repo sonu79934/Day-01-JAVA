41e17ca29f0c51ea0ee3cdcc3847a6f60764e740
sonu79934 <yogasritha8@gmail.com>
2024-12-24 10:03:45 +0530

    Add files via upload

---
+++ personage.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class personage{
+    public static void main(String args[]){
+        //Accepting string
+        Scanner sc=new Scanner(System.in); 
+        String a=sc.next();
+        String b=sc.next();
+        System.out.println("The name of the person is "+b+" and the age of the person is "+a+",a,b");
+    }
+}
